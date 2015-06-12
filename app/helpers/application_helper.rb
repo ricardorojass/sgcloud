8e00e5a792c99a1e35e8f1bc93a9d358be2ea908
Ricardo Rojas <reddrum7@gmail.com>
2015-06-11 19:54:50 -0500

    fix yield title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
-  def title
-    content_for?(:title) ? yield(:title) : "SG Cloud"
+  # Return the full title on a pair-page.
+  def full_title(page_title = '')
+    base_title = "SG Cloud"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
   end
 end
